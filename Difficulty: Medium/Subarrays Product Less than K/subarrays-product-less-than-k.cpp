389b60cbdf3707276c55bf3e9b74277e092b73df
theakshat123 <151052142+theakshat123@users.noreply.github.com>
2026-06-29 05:20:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays Product Less than K/subarrays-product-less-than-k.cpp
+++ Difficulty: Medium/Subarrays Product Less than K/subarrays-product-less-than-k.cpp
@@ -2,24 +2,18 @@ class Solution {
   public:
     int countSubarray(vector<int>& arr, int k) {
         // code here
-        int low=0,high=0;
-        long long p = 1;int res=0;
-        if(k<=1){
+      int low=0,high=0,res=0;
+      int n=arr.size(); long long p=1;
+      if(k<=1){
             return 0;
         }
-        
-        int n=arr.size();
-        for(high=0;high<n;high++){
-            p*=arr[high];
-            while(p>=k){
-              p/=arr[low];
-              low++;
-            }
-            
-                int len =high-low+1;
-                res+=len;
-            
-        } 
-        return res;
+      for(high=0;high<n;high++){
+         p*=arr[high];
+         while(p>=k){
+             p/=arr[low];
+             low++;
+         }int len=high-low+1;
+         res+=len;
+      } return res;
     }
 };
