d3905244891f06d2b90bcb9bb43650477ac55f92
theakshat123 <151052142+theakshat123@users.noreply.github.com>
2026-07-09 00:35:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted Insert Position/sorted-insert-position.cpp
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.cpp
@@ -2,18 +2,17 @@ class Solution {
   public:
     int searchInsertK(vector<int> &nums, int target) {
         // code here
-         int start =0;int end=nums.size()-1;
-       while(start<=end){
-        int mid =start + ( end - start)/2 ;
-        if(nums[mid] == target){
-            return mid;
-          
-        }else if(nums[mid]<target){
-           start = mid + 1;
-        }
-        else
-        
-        end = mid - 1;
-       } return start;
-    }
-};
+         int low=0,high=nums.size()-1;
+         while(low<=high){
+             int mid=low+(high-low)/2;
+             if(nums[mid]==target){
+                 return mid;
+             }
+             else if(nums[mid]<target){
+                 low=mid+1;
+             }
+             else{
+                 high=mid-1;
+             }
+         } return low;
+    }};
