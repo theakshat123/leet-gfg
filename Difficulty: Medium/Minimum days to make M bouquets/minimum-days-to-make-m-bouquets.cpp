21ac0f03ed66ee1d3cebe21cff684c2a89e6be55
theakshat123 <151052142+theakshat123@users.noreply.github.com>
2026-07-09 02:12:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum days to make M bouquets/minimum-days-to-make-m-bouquets.cpp
+++ Difficulty: Medium/Minimum days to make M bouquets/minimum-days-to-make-m-bouquets.cpp
@@ -2,35 +2,34 @@ class Solution {
   public:
     int minDaysBloom(vector<int>& arr, int k, int m) {
         // Code here
-        int n=arr.size();
-        if((long long)m*k>n){
-            return -1;
-        }
-        int low=*min_element(arr.begin(),arr.end());
-        int high=*max_element(arr.begin(),arr.end());
-        int ans=high;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int flower=0,bonquet=0;
-            for(int i=0;i<n;i++){
-                if(arr[i]<=mid){
-                    flower++;
-                }
-                else{
-                    flower=0;
-                }
-                if(flower==k){
-                    bonquet++;
-                    flower=0;
-                }
-            }
-            if(bonquet>=m){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        } return ans;
+      int n=arr.size();
+      if((long long) k*m >n){
+          return -1;
+      }
+      int low = *min_element(arr.begin(),arr.end());
+      int high = *max_element(arr.begin(),arr.end());
+      int ans= high;
+      while(low<=high){
+          int mid = low+(high-low)/2;
+          int flower=0,bonquet=0;
+          for(int i=0;i<n;i++){
+              if(arr[i]<=mid){
+                  flower++;
+              }
+              else{
+                  flower=0;
+              }
+              if(flower==k){
+                  bonquet++;
+                  flower=0;
+              }
+          }
+          if(bonquet>=m){
+             ans=mid; high=mid-1; 
+          }
+          else{
+              low=mid+1;
+          }
+      } return ans;
     }
 };
