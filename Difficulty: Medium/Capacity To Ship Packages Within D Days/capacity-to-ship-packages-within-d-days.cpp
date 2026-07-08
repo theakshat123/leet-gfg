ca30404336cf43a3bdc5fc7a2c407e7c0fb59cbe
theakshat123 <151052142+theakshat123@users.noreply.github.com>
2026-07-09 02:00:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.cpp
+++ Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.cpp
@@ -2,27 +2,30 @@ class Solution {
   public:
     int leastWeightCapacity(vector<int>& arr, int D) {
         // code here
-       int low=*max_element(arr.begin(),arr.end());
-       int high=accumulate(arr.begin(),arr.end(),0);
-       int ans=high;
-       while(low<=high){
-           int mid=low+(high-low)/2;
-           int day=1,load=0;
-           for(int i=0;i<arr.size();i++){
-               if(load+arr[i]<=mid){
-                   load+=arr[i];
-               }
-               else{
-                   day++;
-                   load=arr[i];
-               }
-           } if(day<=D){
-               ans=mid;
-               high=mid-1;
-           }
-           else{
-               low=mid+1;
-           }
-       } return ans;
-    }
-};
+        int n=arr.size();
+        int low=*max_element(arr.begin(),arr.end());
+        int high=accumulate(arr.begin(),arr.end(),0);
+        int ans=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int load=0,day=1;
+            for(int i=0;i<n;i++){
+                if(load+arr[i]<=mid){
+                    load+=arr[i];
+                    
+                }
+                else{
+                    day++;
+                   load =arr[i];
+                }
+            }
+            if(day<=D){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        } return ans;
+        
+    }};
